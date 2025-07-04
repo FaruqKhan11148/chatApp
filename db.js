aee4fb0cb36fcdb92171221770e3b58b7ac2ba49
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2025-07-04 23:12:51 +0530

    updated

--- db.js
+++ db.js
@@ -2,18 +2,19 @@ const mysql =require("mysql2/promise");
 
 async function initDb(){
   try{
-    const connection =await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'Faruq@11148',
-    database: 'temp',
-});
+        const connection =await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'Faruq@11148',
+        database: 'temp',
+    });
 
-console.log("connected to db");
-return connection;
-} catch(err){
-    console.err("connection failed!");
-}
+        console.log("connected to db");
+        return connection;
+    } catch(err){
+        console.err("connection failed!");
+    }
 }
 
+// export function
 module.exports=initDb();
